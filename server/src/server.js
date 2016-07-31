f03c81e1efe148496982fafd8b8d89c19d217f90
migcoder <chenhuikai@shinetechchina.com>
2016-07-31 18:20:53 +0800

    Add Intellisense for nodejs and express in vscode.

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import config from './config';
+
 import router from './router/router';
+
+import http from 'http';
 
 const appServer = express();
 router.config(appServer);
 
 appServer.listen(config.port, function () {
     console.log('Server is running!');
+    
 })
